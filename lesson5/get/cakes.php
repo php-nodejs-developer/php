503f62e746ff801d508abf913effa809b5e21b04
nikita <adrbrug@gmail.com>
2022-06-07 18:54:59 +0300

    Занятие 5: файлы лекции++

---
+++ lesson5/get/cakes.php
@@ -0,0 +1,18 @@
+<?php
+// по каждому тортику выводить:
+// название (name),
+// изображение (main_img),
+// стоимость (price) и валюту (currency)
+
+$cakes = require_once 'data.php';
+?>
+<!doctype html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+
+</body>
+</html>
